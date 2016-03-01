a7c01701232b64b4912287ec8dbcc4f24e438d74
William Patruno <w.patruno@rubika-edu.com>
2016-03-01 14:28:52 +0100

    Network Killed

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+    Motherbase mBase;
+
+	// Use this for initialization
+	void Start () {
+        CreateBase();
+
+    }
+
+    void CreateBase()
+    {
+        GameObject objbase = Resources.Load("BaseBuilding") as GameObject;
+        objbase.transform.position = transform.position;
+        objbase.transform.parent = transform;
+        objbase.AddComponent<Motherbase>();
+        mBase = objbase.GetComponent<Motherbase>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
