88d43f60a1ac7c550f174555eb6216c712615ed2
unknown <r.mathieu@J001-0140-15.etudiant.rubika-edu.com.local>
2016-03-01 09:03:23 +0100

    Test network UNET

---
+++ Assets/Scripts/Entities/Player.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour {
+
+    /*
+    Variable toujours mise a jour sur les clients
+    [SyncVar]
+    public float  myVar;
+    */
+
+    // Use this for initialization
+    void Start () {
+	    
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (!isLocalPlayer) return;
+
+        float h = Input.GetAxis("Horizontal"), v = Input.GetAxis("Vertical");
+        transform.Translate(h, 0.0f, v);
+
+    }
+    /*
+    Envoyer aux clients une action (exemple : animation / sound / etc.)
+
+    void Action()
+    {
+        DoAction;
+        CmdTest();
+    }
+
+    [Command]
+    void CmdTest()
+    {
+        RpcTest();
+    }
+
+    [ClientRpc]
+    void RpcTest()
+    {
+        
+    }*/
+
+    /*
+    Commande du serveur appelé depuis le client
+    
+    [Command]
+    void CmdDo()
+    {
+        NetworkServer.Spawn(GameObject); // Spawn un gameobject chez tous les joueurs
+    }
+    
+    Commande du client appelé depuis le serveur
+    
+    [ClientRpc]
+    void RpcDo()
+    {
+        if (isLocalPlayer)
+        {
+            
+        }
+    }
+    
+    */
+}
