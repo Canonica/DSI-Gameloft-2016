8f7ab975aaa4e86e5b9686665adcd218f65eea0f
William Patruno <w.patruno@rubika-edu.com>
2016-03-04 15:22:55 +0100

    UI

--- Assets/Scripts/Entities/MotherBase/Motherbase.cs
+++ Assets/Scripts/Entities/MotherBase/Motherbase.cs
@@ -173,8 +173,8 @@ public class Motherbase : Entity
             
             textCurrentNbOfUnits[0].text = currentNbOfUnits[0] + "/" + maxNbOfUnits[0];
             textCurrentNbOfUnits[1].text = currentNbOfUnits[1] + "/" + maxNbOfUnits[1];
-            textCurrentNbOfUnits[2].text = currentNbOfUnits[1] + "/" + maxNbOfUnits[2];
-            textCurrentNbOfUnits[3].text = currentNbOfUnits[1] + "/" + maxNbOfUnits[3];
+            textCurrentNbOfUnits[2].text = currentNbOfUnits[2] + "/" + maxNbOfUnits[2];
+            textCurrentNbOfUnits[3].text = currentNbOfUnits[3] + "/" + maxNbOfUnits[3];
         }
 
         _lifeImage.fillAmount = (float)((float)_life / (float)_lifeMax);
