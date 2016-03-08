7a7ef01b5a3b98e8a99f4f34480c85b564e1679c
unknown <r.mathieu@J001-0140-15.etudiant.rubika-edu.com.local>
2016-03-08 14:37:41 +0100

    Hornet Upgrade

---
+++ Assets/Scripts/Effect/Upgrade/HornetUpgrade.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HornetUpgrade : Upgrade {
+
+    public override void LevelOne(Unit unit)
+    {
+        base.LevelOne(unit);
+        UnitRush rushUnit = (UnitRush)unit;
+        if (rushUnit)
+        {
+            rushUnit.lifeSteal = true;
+        }
+    }
+
+    public override void LevelTwo(Unit unit)
+    {
+        base.LevelTwo(unit);
+        UnitRush rushUnit = (UnitRush)unit;
+        if (rushUnit)
+        {
+            rushUnit.rangedAttack = true;
+        }
+    }
+
+    public override void LevelThree(Unit unit)
+    {
+        base.LevelThree(unit);
+        UnitRush rushUnit = (UnitRush)unit;
+        if (rushUnit)
+        {
+            rushUnit.bloodyRash = true;
+        }
+    }
+}
